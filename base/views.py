297b4da34a8a0f1db7e6a3aed9e93403f8f799ed
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-08-13 00:01:06 +0700

    update

--- base/views.py
+++ base/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
-from typing import Any
+# from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from .models import Notes
+# from .models import Notes
 from django.http import Http404
 from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView,DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime
+from django.views.generic import TemplateView
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 class HomeView(TemplateView):
-    template_name = 'main.html'
+    template_name = 'maintemplates/main.html'
     extra_context = {'today': datetime.today()}
 
 # Create your views here.
