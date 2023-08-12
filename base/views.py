9e6eb4b73a240b88a97eec9a9235bb91effca1e1
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-08-12 23:33:48 +0700

    update

--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from .models import Notes
+from django.http import Http404
+from django.http.response import HttpResponseRedirect
+from django.views.generic import CreateView,DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 # Create your views here.
