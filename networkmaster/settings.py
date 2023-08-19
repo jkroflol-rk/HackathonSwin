d2476e68e8282076865764c50f27b323be738fe0
Nhat Le <tori.tuan@gmail.com>
2023-08-19 10:31:44 +0700

    rename project file for clarity

--- networkmaster/settings.py
+++ networkmaster/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tree.urls'
+ROOT_URLCONF = 'networkmaster.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tree.wsgi.application'
+WSGI_APPLICATION = 'networkmaster.wsgi.application'
 
 
 # Database
