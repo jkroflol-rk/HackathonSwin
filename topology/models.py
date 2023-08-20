431c973a1e81b2af96d23de064ee7de6363e8e7a
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2023-08-20 19:54:39 +0700

    update

--- topology/models.py
+++ topology/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class SwitchConfig(models.Model):
+    input_data = models.TextField()
+    output = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- topology/views.py
+++ topology/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from typing import Any
+from django.db.models.query import QuerySet
+# from .form import MyForm
+from .models import SwitchConfig
+from django.http import Http404
+from django.http.response import HttpResponseRedirect
+from django.views.generic import CreateView,DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+class CreateConfigCLI(CreateView):
+    model = SwitchConfig
+    fields = ['input_data', 'output']
+    success_url = ''
+    template_name = ''
+
+
