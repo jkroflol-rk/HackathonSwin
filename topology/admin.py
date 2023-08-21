45ee9e80dec9036c823c8e9a6b48b7f5ffc88e7f
Nhat Le <tori.tuan@gmail.com>
2023-08-21 19:07:08 +0700

    add model to admin page

--- topology/admin.py
+++ topology/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ('input_data', 'output', 'timestamp')
+
+admin.site.register(models.SwitchConfig, ConfigAdmin)
