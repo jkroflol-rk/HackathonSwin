89cd7c0afe6e8975a5638b0627ef59c7d43e6f62
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2023-08-29 22:04:33 +0700

    udpate

--- topology/views.py
+++ topology/views.py
@@ -32,17 +32,47 @@ class ConfigDetail(DetailView):
     template_name = "configdetail.html"
     # login_url = "/login"
 from .forms import SerialConnectionForm
+import serial
 import time
 def connectSerial(input, output):
     # creating your serial object
-    a = int(input)
-    b = a * a 
-    # while True:
-    #     fdsf
-    serial_data = SwitchConfig(input_data = input, output=b) #output = Switch# enable
-    serial_data.save()
-    return b
-# Press the green button in the gutter to run the script.
+    ser = serial.Serial(
+        port='COM6',  # COM is on windows, linux is different
+        baudrate=9600,  # many different baudrates are available
+        parity='N',  # no idea
+        stopbits=1,
+        bytesize=8,
+        timeout=0.1  # 8 seconds seems to be a good timeout, may need to be increased
+    )
+
+    # open your serial object
+    ser.isOpen()
+    # in this case it returns str COM3
+    print(ser.name)
+    # first command (hitting enter)
+
+    # wait a sec
+    time.sleep(0.1)
+    ser.inWaiting()
+    # get the response from the switch
+    input_data = ser.read(225)  # (how many bytes to limit to read)
+    input_data = input_data.decode("utf-8", "ignore")
+    # print response
+    print(input_data)
+    # create a loop
+    while 1:
+        command = input('')
+        command = str.encode(command+'\r\n')
+        ser.write(command)
+        time.sleep(0.1)
+        ser.inWaiting()
+        output = ser.read(225)
+        output = output.decode("utf-8","ignore")
+        print(output)
+        # serial_data = SwitchConfig(output=output) #output = Switch# enable
+        # serial_data.save()
+
+
 
 def connect_serial(request):
     if request.method == 'POST':
