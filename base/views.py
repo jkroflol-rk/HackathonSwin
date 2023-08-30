d8f371a7f7dd6dd4ff3118ac872fc4d9907dc19b
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-08-30 16:29:09 +0700

    update

--- base/views.py
+++ base/views.py
@@ -103,6 +103,7 @@ class RegistrationForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         label='Username',
+        
         widget=forms.TextInput(
             attrs = {
                 'placeholder': 'Enter username',
--- topology/forms.py
+++ topology/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class SerialConnectionForm(forms.Form):
     com_port = forms.CharField(label='Input', max_length=10)
     baud_rate = forms.IntegerField(label='Baud Rate', initial=9600)
+    
