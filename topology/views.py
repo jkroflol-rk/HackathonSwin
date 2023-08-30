c2be0a2fd0d491dade79015e16bd92988e66a5c9
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-08-30 22:30:06 +0700

    update

--- topology/views.py
+++ topology/views.py
@@ -100,14 +100,12 @@ def connect_serial(request):
         form = SerialConnectionForm(request.POST)
         if form.is_valid():
             com_port = form.cleaned_data['com_port']
-            baud_rate = form.cleaned_data['baud_rate']
             history =  connectserial(com_port, request)
             return redirect('connect_serial')  # Redirect to a success page
     else:
 
         form = SerialConnectionForm()
         if request.session.get('history') != None:
-            finaloutput = request.session.get('output')
             history = request.session.get('history')
         else:
             history = "Nothing"
