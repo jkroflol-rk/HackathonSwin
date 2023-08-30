8e8c330df712d37cdac839ffcf8c12cd99772df2
Nhat <tori.tuan@gmail.com>
2023-08-31 03:43:39 +0700

    add 2 more template topo

--- cyto/views.py
+++ cyto/views.py
@@ -40,6 +40,17 @@ def star_cyto(request):
 class input(TemplateView):
     template_name = 'form.html'
 
+class choice(TemplateView):
+    template_name = 'choice.html'
+
+
+class inputhome(TemplateView):
+    template_name = "home.html"
+
+class inputschool(TemplateView):
+    template_name = "school.html"
+
+
 def process(request):
     department = int(request.POST['departments'])
     names = request.POST.getlist('name[]')
@@ -61,6 +72,43 @@ def process(request):
         json.dump(output_array, json_file)
 
     return redirect('/cyto')
+
+def process_home(request):
+    wifi = int(request.POST['wifi'])
+    pc = int(request.POST['pc'])
+    printer = int(request.POST['printer'])
+
+    output = dict(wifinum = wifi, pcnum = pc, printernum = printer)
+
+    with open('./static/js/outputhome.json', 'w') as json_file:
+        json.dump(output, json_file)
+
+    return redirect('/cyto/star')
+
+def process_school(request):
+    department = int(request.POST['departments'])
+    students = int(request.POST['student'])
+    names = request.POST.getlist('name[]')
+    vlans = request.POST.getlist('host[]')
+
+    output_array = []
+
+    for i in range(0, department):
+        output = dict(name = names[i], host=vlans[i])
+        output_array.append(output)
+
+    output2 = dict(student = students, room = output_array)
+
+    if 'array' not in request.session:
+        request.session['array'] = []
+
+    request.session['array'].extend(output2)
+    request.session.modified = True
+
+    with open('./static/js/outputschool.json', 'w') as json_file:
+        json.dump(output2, json_file)
+
+    return redirect('/cyto/school')
 
 def saveJsonFile(json, filename):
     # open text file
