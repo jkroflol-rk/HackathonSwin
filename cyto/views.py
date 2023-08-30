7af8ec7ffa05847f9e14436156706927cd54e892
thanh <dou.peter17@gmail.com>
2023-08-30 15:29:20 +0700

    cyto final

--- cyto/views.py
+++ cyto/views.py
@@ -2,17 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, FileResponse
 from cyto import final
 from django.template import loader
-def index(request):
+def cyto(request):
     networkDevice, portDevice, vlan = final.cyto_json()
     saveJsonFile(networkDevice, "networkdevice.json")
     saveJsonFile(portDevice, "portdevice.json")
     saveJsonFile(vlan, "vlan.json")
-    template = loader.get_template('index.html')
+    template = loader.get_template('cyto.html')
     return HttpResponse(template.render())
-def images(request):
-    img = open("/images/3650.png", "rb")
-    response = FileResponse(img)
-    return response
 def saveJsonFile(json, filename):
     # open text file
     text_file = open("./static/js/"+filename, "w")
@@ -20,6 +16,5 @@ def saveJsonFile(json, filename):
     text_file.write(json)
     # close file
     text_file.close()
-from django.shortcuts import render
 
-# Create your views here.
+
--- networkmaster/settings.py
+++ networkmaster/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'base',
     'topology',
+    'cyto',
 ]
 
 MIDDLEWARE = [
