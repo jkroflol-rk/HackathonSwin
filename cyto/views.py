5dd59a847615ea27d6560975d6bdc485e0be3fa2
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-08-30 21:02:36 +0700

    update

--- cyto/views.py
+++ cyto/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, FileResponse
 from cyto import final, school, star
 from django.template import loader
-from models import VlanConfig, WifiConfig
+from .models import VlanConfig, WifiConfig
 def cyto(request):
     networkDevice, portDevice, vlan = final.cyto_json()
     saveJsonFile(networkDevice, "networkdevice.json")
